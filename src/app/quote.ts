704c07608b3cf23604fbe9eb2c3635ce73e93e79
Justusm10 <justusm10@gmail.com>
2019-12-02 14:10:31 +0300

    edit license

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,3 +2,4 @@ export class Quote {
     
     constructor(public id:number, public quote:string,public publisher: string, public author: string, public upvote: number, public downvote: number, public myDate:Date){}
 }
+ 
